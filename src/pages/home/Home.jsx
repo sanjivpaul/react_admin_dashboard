f50df04619e2722319a965b2f7babcb5e31fade0
Sanjiv Paul <paulsanjiv81@gmail.com>
2023-05-20 15:28:50 +0530

    featuredInfo component created🚀

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,10 @@
+import FeaturedInfo from "../../components/featuredInfo/FeaturedInfo"
+import "./home.css"
+
+export default function Home() {
+    return (
+        <div className='homePage'>
+            <FeaturedInfo />
+        </div>
+    )
+}
