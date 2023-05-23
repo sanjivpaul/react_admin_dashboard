138fb7eb8d1f7638b419a8aa848dde2671871c9d
Sanjiv Paul <paulsanjiv81@gmail.com>
2023-05-23 17:55:04 +0530

    set react-router-dom

--- src/App.js
+++ src/App.js
@@ -2,16 +2,23 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar"
 import "./app.css"
 import Home from "./pages/home/Home";
+// import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+import UserList from "./pages/userList/UserList";
 
 function App() {
   return (
-    <div>
+    <>
       <Topbar />
       <div className="container">
         <Sidebar />
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/users" element={<UserList />} />
+        </Routes>
       </div>
-    </div>
+    </>
+
   );
 }
 
